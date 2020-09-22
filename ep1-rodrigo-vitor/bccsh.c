36719f03481cfc090909f8d7f8a8c874ec7eea61
VitorOak <vitor27melo@usp.br>
2020-09-22 20:02:45 -0300

    hotfix include sys/wait

--- ep1-rodrigo-vitor/bccsh.c
+++ ep1-rodrigo-vitor/bccsh.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
 
 int leEntrada(char *entrada){
     char *buf;
@@ -33,14 +34,14 @@ int executaEntrada(char **entrada_parseada){
         pid = fork();
         if (pid == -1){
             printf("/nFalha ao fazer fork");
-            return;
+            return 1;
         } else if (pid == 0){
             if (execvp(entrada_parseada[0], entrada_parseada) < 0)
                 printf("/nO programa não pode ser aberto");
         } else {
             // Espera o programa child terminar
             wait(NULL);
-            return;
+            return 0;
         }
         exit(0);
     }
