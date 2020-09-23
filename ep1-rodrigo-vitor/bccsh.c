e79d2f34838933253683b80ee36ecbfea41b9ddf
VitorOak <vitor27melo@usp.br>
2020-09-23 16:21:49 -0300

    Shell finalizado :

--- ep1-rodrigo-vitor/bccsh.c
+++ ep1-rodrigo-vitor/bccsh.c
@@ -4,6 +4,30 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+
+void _mkdir(char **entrada_parseada){
+    // Chama a system call mkdir
+    if(mkdir(entrada_parseada[1], 0777) == -1)
+        printf("Falha ao criar diretório: %s\n", strerror(errno));
+}
+
+void _kill(char **entrada_parseada){
+    // Chama a system call kill
+    pid_t pid = atoi(entrada_parseada[2]);
+    if(kill(pid, SIGKILL) == -1)
+        printf("Falha em matar o processo: %s\n", strerror(errno));
+}
+
+int _ln(char **entrada_parseada){
+    // Código para criar pasta
+    printf("%s%s%s%s",entrada_parseada[0],entrada_parseada[1], entrada_parseada[2],entrada_parseada[3]);
+    if(symlink(entrada_parseada[2],entrada_parseada[3]) == -1)
+        printf("Falha em criar link simbólico: %s\n", strerror(errno));
+}
 
 int leEntrada(char *entrada){
     char *buf;
@@ -19,7 +43,7 @@ int leEntrada(char *entrada){
 
 void parseEntrada(char *entrada, char **entrada_parseada){
     int i;
-    for (i=0; i<3 ; i++){
+    for (i=0; i<=3 ; i++){
         entrada_parseada[i] = strsep(&entrada, " ");
         if (entrada_parseada[i] == NULL)
             break;
@@ -29,7 +53,11 @@ void parseEntrada(char *entrada, char **entrada_parseada){
 int executaEntrada(char **entrada_parseada){
     pid_t pid;
     if (!strcmp("mkdir",entrada_parseada[0])){
-        printf("banana");
+        _mkdir(entrada_parseada);
+    }else if (!strcmp("kill",entrada_parseada[0]) && !strcmp("-9",entrada_parseada[1])){
+        _kill(entrada_parseada);
+    }else if (!strcmp("ln",entrada_parseada[0]) && !strcmp("-s",entrada_parseada[1])){
+        _ln(entrada_parseada);
     }else{
         pid = fork();
         if (pid == -1){
@@ -40,13 +68,15 @@ int executaEntrada(char **entrada_parseada){
                 printf("O programa '%s' não pôde ser aberto.\n", entrada_parseada[0]);
         } else {
             // Espera o programa child terminar
-            wait(NULL);
+            waitpid(-1,NULL,0);
             return 0;
         }
         exit(0);
     }
     return 0;
 }
+
+
 
 int main(){
     // Recupera o nome do usuário
