de5c943d3ac03092b4e884e3e01c24614afb7bcb
VitorOak <vitor27melo@usp.br>
2020-09-23 21:00:37 -0300

    Adicionei arquivos pra parte 1.2

---
+++ ep1-rodrigo-vitor/ep1.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]){
+    char texto[2000];
+    while(read(0, texto, 2000) > 0)
+    // Executa para cada linha
+    return 0;
+}
